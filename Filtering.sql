2fa86d68250d6f4b249cf64dd6990595b185346a
Gopika Ajikumar <gopikaajikumar213@gmail.com>
2025-12-02 10:39:06 +0530

    Add SQL queries for student filtering
    Added various SQL queries to filter students based on age, marks, and name patterns.

--- Filtering.sql
+++ Filtering.sql
@@ -1,1 +1,28 @@
 
+SELECT * FROM students          #checks both the statements and runs only if both are true
+WHERE age = 20 AND marks > 80;
+
+
+SELECT * FROM students        # either one of the statement must be true
+WHERE age = 19 OR age = 22;
+
+
+SELECT * FROM students           # in the middle of the two numbers or range
+WHERE marks BETWEEN 70 AND 90;
+
+
+SELECT * FROM students        # in is an alternative for OR, when there is a need of multiple OR statement
+WHERE age IN (19, 21);
+
+
+SELECT * FROM students    # same as cheking the equivalancy of the text, here a% means text starting with the letter "a"
+WHERE name LIKE 'A%';
+
+
+SELECT * FROM students       #  %a means text that ends with the letter "a"
+WHERE name LIKE '%a';
+
+
+SELECT * FROM students
+WHERE name LIKE '%a%';     # text that containing the letter "a"
+
