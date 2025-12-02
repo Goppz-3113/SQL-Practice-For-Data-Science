4a61a0d845ba4f19c75b2bc270c74e1838d7032f
Gopika Ajikumar <gopikaajikumar213@gmail.com>
2025-12-02 11:33:47 +0530

    Add SQL queries for grouping students by age and gender

--- grouping.sql
+++ grouping.sql
@@ -1,1 +1,21 @@
+-- 
+SELECT age, COUNT(*)   #Group students by age and gives the count
+FROM students
+GROUP BY age;
+
+-- 
+SELECT gender, COUNT(*)    # Group students by gender and gived the count
+FROM students
+GROUP BY gender;
+
+-- 
+SELECT age, AVG(marks)   #Average marks by age
+FROM students
+GROUP BY age;
+
+-- 
+SELECT age, AVG(marks)  #Only show high scoring age groups
+FROM students
+GROUP BY age
+HAVING AVG(marks) > 75;
 
