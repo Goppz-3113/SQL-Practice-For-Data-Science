89aeb7b69aa3f026069858beeb9821b9621143f6
Gopika Ajikumar <gopikaajikumar213@gmail.com>
2025-12-11 12:14:16 +0530

    Add SQL queries for student marks analysis
    Added SQL queries to find students based on marks criteria.

--- subqueries.sql
+++ subqueries.sql
@@ -1,1 +1,21 @@
+==== Student with highest marks ====
+  
+SELECT *
+FROM students
+WHERE marks = (SELECT MAX(marks) FROM students);
 
+==== Students above class average ====
+  
+SELECT *
+FROM students
+WHERE marks > (SELECT AVG(marks) FROM students);
+
+==== Students whose marks are above their gender average ===
+SELECT *
+FROM students s
+WHERE marks > (
+    SELECT AVG(marks)
+    FROM students
+    WHERE gender = s.gender  # s.gender = gender from outer query student , gender= gender inside subquery student list
+);                           # s is just a shortcut name  for the table students
+                 
